6a3f32d36a340d397866fde679b8350eac443007
WS31 <esiosezequiel@gmail.com>
2024-09-28 11:29:29 +0800

    mao ni akong ge copy

---
+++ mycopytask.py
@@ -0,0 +1,25 @@
+
+
+age= [-12,13,-14,15,-16]
+
+for data in age:
+    if data < 0:
+        print(str(data)+ "negative")
+    elif datage > 0:
+         print(str(datage)+ "positive")
+
+
+
+
+
+
+username = ["juan", "pedro","james", "sagaad", "fuego"]
+for data in username:
+     print("james cute")
+        break;
+else:
+       print("james none")
+      
+            
+   
+   
